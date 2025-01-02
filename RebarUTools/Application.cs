783f0ffb8923912ea21916efd3e1fe960124374d
Marcin Wolinski <mwo@woodthilsted.com>
2025-01-02 14:34:22 +0100

    Add project files.

---
+++ RebarUTools/Application.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+namespace RebarUTools
+{
+    public class Application : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
